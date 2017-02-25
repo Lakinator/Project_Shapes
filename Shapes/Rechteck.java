9af27e944fc5553962e6efd82d2b30353dc1fe1e
Lukas S <SchalkLukas20@gmail.com>
2017-02-25 15:46:18 +0100

    Update Rechteck.java

--- Shapes/Rechteck.java
+++ Shapes/Rechteck.java
@@ -9,7 +9,6 @@ import javax.swing.*;
 
 public class Rechteck implements Shape {
     private final ShapeType type = ShapeType._2D;
-    private final static int varLength = 5;
     private double width = 0, length = 0, umfang = 0, inhalt = 0, diagonale = 0;
 
 
@@ -19,13 +18,13 @@ public class Rechteck implements Shape {
     }
 
     @Override
-    public int getVarLength() {
-        return varLength;
+    public String[] getVarNames() {
+        return new String[]{"Breite", "Länge", "Umfang", "Inhalt", "Diagonale"};
     }
 
     @Override
-    public String[] getVarNames() {
-        return new String[]{"Breite", "Länge", "Umfang", "Inhalt", "Diagonale"};
+    public double[] getVariables() {
+        return new double[]{width, length, umfang, inhalt, diagonale};
     }
 
     @Override
@@ -94,60 +93,26 @@ public class Rechteck implements Shape {
         return false;
     }
 
-
+    @Override
     public void updateValues(final JTextArea[] textAreas) {
-        setWidth( Double.parseDouble(textAreas[0].getText()) );
-        setLength( Double.parseDouble(textAreas[1].getText()) );
-        setUmfang( Double.parseDouble(textAreas[2].getText()) );
-        setInhalt( Double.parseDouble(textAreas[3].getText()) );
-        setDiagonale( Double.parseDouble(textAreas[4].getText()) );
+        width = Double.parseDouble(textAreas[0].getText());
+        length = Double.parseDouble(textAreas[1].getText());
+        umfang = Double.parseDouble(textAreas[2].getText());
+        inhalt = Double.parseDouble(textAreas[3].getText());
+        diagonale = Double.parseDouble(textAreas[4].getText());
     }
 
+    @Override
+    public String errorMsg() {
+        return "Mindestens Länge oder Breite und Umfang, Inhalt oder Diagonale benötigt";
+    }
+
+    @Override
     public void resetAll() {
-        setWidth(0);
-        setLength(0);
-        setInhalt(0);
-        setUmfang(0);
-        setDiagonale(0);
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public double getUmfang() {
-        return umfang;
-    }
-
-    public void setUmfang(double umfang) {
-        this.umfang = umfang;
-    }
-
-    public double getInhalt() {
-        return inhalt;
-    }
-
-    public void setInhalt(double inhalt) {
-        this.inhalt = inhalt;
-    }
-
-    public double getDiagonale() {
-        return diagonale;
-    }
-
-    public void setDiagonale(double diagonale) {
-        this.diagonale = diagonale;
+        width = 0;
+        length = 0;
+        umfang = 0;
+        inhalt = 0;
+        diagonale = 0;
     }
 }
